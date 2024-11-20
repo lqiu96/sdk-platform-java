855da6009e3db269ae3d66e29c963ecaffa1d3d7
Lawrence Qiu <lawrenceqiu@google.com>
2024-11-20 01:04:11 -0500

    feat: Add a default exclusion file

--- .kokoro/presubmit/downstream-protobuf-compatibility.sh
+++ .kokoro/presubmit/downstream-protobuf-compatibility.sh
@@ -38,5 +38,5 @@ for repo in ${REPOS_UNDER_TEST//,/ }; do # Split on comma
   # Perform testing on last release, not HEAD
   last_release=$(find_last_release_version "$repo")
   repo_name=$(echo "$repo" | cut -d '-' -f 2)
-  mvn exec:java -Dexec.mainClass="com.google.cloud.tools.opensource.classpath.LinkageCheckerMain" -Dexec.args="-r --artifacts com.google.cloud:google-cloud-${repo_name}:${last_release}"
+  mvn exec:java -Dexec.mainClass="com.google.cloud.tools.opensource.classpath.LinkageCheckerMain" -Dexec.args="-r --artifacts com.google.cloud:google-cloud-${repo_name}:${last_release} -e default_exclusions.txt"
 done
